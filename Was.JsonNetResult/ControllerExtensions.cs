60f942e9b2bd12a064ad976611082caf1df46ab7
Patryk Wąsiewicz <pat.wasiewicz@gmail.com>
2016-02-23 21:49:56 +0100

    First commit

---
+++ Was.JsonNetResult/ControllerExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Was.JsonNetResult
+{
+    using System.Text;
+    using System.Web.Mvc;
+
+    public static class ControllerExtensions
+    {
+        public static JsonResult JsonNet(this Controller controller, object data, string contentType, Encoding contentEncoding, JsonRequestBehavior behavior)
+        {
+            return new JsonNetResult
+            {
+                Data = data,
+                ContentType = contentType,
+                ContentEncoding = contentEncoding,
+                JsonRequestBehavior = behavior
+            };
+        }
+    }
+}
---
+++ Was.JsonNetResult/JsonNetResult.cs
@@ -0,0 +1,47 @@
+﻿namespace Was.JsonNetResult
+{
+    using System;
+    using System.IO;
+    using System.Web;
+    using System.Web.Mvc;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Serialization;
+
+    public class JsonNetResult : JsonResult
+    {
+        public JsonNetResult()
+        {
+            this.Settings = new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+        }
+
+        public JsonSerializerSettings Settings { get; private set; }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException("context");
+            if (this.JsonRequestBehavior == JsonRequestBehavior.DenyGet && string.Equals(context.HttpContext.Request.HttpMethod, "GET", StringComparison.OrdinalIgnoreCase))
+                throw new InvalidOperationException("JSON GET is not allowed");
+
+            HttpResponseBase response = context.HttpContext.Response;
+            response.ContentType = string.IsNullOrEmpty(this.ContentType) ? "application/json" : this.ContentType;
+
+            if (this.ContentEncoding != null)
+                response.ContentEncoding = this.ContentEncoding;
+            if (this.Data == null)
+                return;
+
+            var scriptSerializer = JsonSerializer.Create(this.Settings);
+
+            using (var sw = new StringWriter())
+            {
+                scriptSerializer.Serialize(sw, this.Data);
+                response.Write(sw.ToString());
+            }
+        }
+    }
+}
